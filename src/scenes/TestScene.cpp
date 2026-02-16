d2bc11b252a38b9fc8bf12beba9a6768db36499b
Wu ChengYan <710510690@qq.com>
2026-02-17 00:37:16 +0800

    wasd to controll moving

--- src/scenes/TestScene.cpp
+++ src/scenes/TestScene.cpp
@@ -1,4 +1,6 @@
 #include "TestScene.hpp"
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
 TestScene::TestScene(sf::RenderWindow& window)
     : window(window) {
     //ctor
@@ -13,7 +15,6 @@ update(float deltaTime){
 }
 void TestScene::render(sf::RenderWindow& window){
     //render
-    
     sf::CircleShape shape( 100.f );
     shape.setFillColor( sf::Color::Green );
     shape.setPosition(Pos);
@@ -21,6 +22,27 @@ void TestScene::render(sf::RenderWindow& window){
 }
 void TestScene::handleEvent(const sf::Event& event){
     //handle event
+    if (const auto* keyPressed = event.getIf<sf::Event::KeyPressed>())
+    {
+        switch (keyPressed->scancode)
+        {
+            case sf::Keyboard::Scancode::W:
+                Pos.y -= 10.f;
+                break;
+            case sf::Keyboard::Scancode::S:
+                Pos.y += 10.f;
+                break;
+            case sf::Keyboard::Scancode::A:
+                Pos.x -= 10.f;
+                break;
+            case sf::Keyboard::Scancode::D:
+                Pos.x += 10.f;
+                break;
+            default:
+                break;
+        }
+    
+    }
     
 }
 void TestScene::onEnter(){
@@ -28,5 +50,4 @@ void TestScene::onEnter(){
 }
 void TestScene::onExit(){
     //on exit
-    
 }
